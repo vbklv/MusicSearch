6b4eaa7f7264f297f44c140fcdb720d182cd2cec
Vladimir Bakalov <vbakalov@gmail.com>
2025-08-02 14:54:07 +0200

    Initial Commit

---
+++ MusicSearch/MusicSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchApp.swift
+//  MusicSearch
+//
+//  Created by Vladimir Bakalov on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicSearchTests/MusicSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchTests.swift
+//  MusicSearchTests
+//
+//  Created by Vladimir Bakalov on 02/08/2025.
+//
+
+import Testing
+@testable import MusicSearch
+
+struct MusicSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
