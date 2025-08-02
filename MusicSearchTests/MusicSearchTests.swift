1bb32ad41a00bf7c46c41d7c3e610053f0fad9f1
Vladimir Bakalov <vbakalov@gmail.com>
2025-08-02 18:38:05 +0200

    Add unit test case

--- MusicSearchTests/MusicSearchTests.swift
+++ MusicSearchTests/MusicSearchTests.swift
@@ -5,13 +5,30 @@
 //  Created by Vladimir Bakalov on 02/08/2025.
 //
 
-import Testing
+import XCTest
 @testable import MusicSearch
 
-struct MusicSearchTests {
+@MainActor
+final class SearchViewModelTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    func testEmptySearchClearsTracks() {
+        // Given a view model with pre-populated tracks
+        let viewModel = SearchViewModel()
+        viewModel.tracks = [
+            Track(
+                trackName: "Track 1",
+                artistName: "Artist 1",
+                artworkUrl100: nil,
+                releaseDate: nil,
+                collectionName: nil
+            )
+        ]
+
+        // When searching with an empty query
+        viewModel.searchText = ""
+        viewModel.search()
+
+        // Then the tracks array should be cleared
+        XCTAssertTrue(viewModel.tracks.isEmpty, "Calling search() with an empty query should clear the tracks array")
     }
-
 }
